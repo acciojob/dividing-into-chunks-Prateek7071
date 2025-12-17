725ef6449317c83b828bd2ee9367af7a98707f56
Prateek <106758759+Prateek7071@users.noreply.github.com>
2025-12-17 20:56:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+
+	let N= n;
+	let cost = []
+	let res = []
+	for(let k of arr){
+		if(N>=k){
+			N-=k
+			cost.push(k)
+      continue;
+		}
+		res.push(cost)
+		cost = []
+		N=n
+		N-=k
+		cost.push(k)
+	}
+	res.push(cost)
+	return res;
 };
 
 const n = prompt("Enter n: ");
